f0b9fe73c2354d066cc64a89b0f8e9d81ccd3fc6
MatMaz5 <matthiasmazet@gmail.com>
2024-11-28 12:01:58 +0100

    Update lancement.jl

--- src/lancement.jl
+++ src/lancement.jl
@@ -13,6 +13,7 @@ function partie98()
             println("Quelle carte voulez-vous jouer ? Entrer son emplacement (1 à 5).")
             println(Jeu_98[i])
             index = nombre()
+            
             if index == 0
                 println("Les cartes nombres ajoute leur valeur au compteur.")
                 println("Le valet ne modifie pas le compteur.")
@@ -20,20 +21,20 @@ function partie98()
                 println("Le roi fixe le compteur à 70.")
                 index = nombre()
             end
+
             while index < 1 || index > 5
                 println("Pas de carte correspondante. Sélectionner un autre nombre.")
                 index = nombre()
             end
+
             print("\e[2J")
             jouer(Jeu_98["compteur"], Jeu_98[i], index, Jeu_98["defausse"], Jeu_98["pioche"])
             if Jeu_98["compteur"][] >= 98 break end
-            
-            ## Pas sûr que cette partie marche
+
             if Jeu_98["pioche"] == ""
                 Jeu_98["pioche"] = shuffle!(Jeu_98["defausse"])
                 Jeu_98["defausse"] = Paquet(Carte[])   
             end
-            ##
             
         end
     end
